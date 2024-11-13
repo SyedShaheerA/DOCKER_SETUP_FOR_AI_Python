8cd4783405e9b54ea54d82d59f7e9e2ab15cb909
Syed Shaheer Ali <75204406+SyedShaheerA@users.noreply.github.com>
2024-11-13 14:50:52 +0300

    Add files via upload

---
+++ app.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+print("check")
+
+
+# In[3]:
+
+
+import spacy 
+import nltk
+from transformers import AutoTokenizer, AutoModel
+print("spacy imported")
+# import nltk
+# print("nltk imported")
+
+
+# In[ ]:
+
+
+
+
